19447b221d8ef810fdc6f750fd1a5f064e87461e
Ritika Bajracharya <89394784+ritikabajracharya08@users.noreply.github.com>
2022-07-03 00:39:22 +0545

    new front page

--- Index.php
+++ Index.php
@@ -5,7 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="assets/images/logokam.png">
+    <link rel="stylesheet" href="zhomepage/styles/Stylesh.css" >
+    <link rel="stylesheet" href="zhomepage/styles/Stylesh.scss" >
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
 
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
     <title>KAM- क्षॆत्र</title>
     <style>
 body, html {
@@ -13,46 +18,156 @@ body, html {
   margin: 0;
   font-family: Arial, Helvetica, sans-serif;
 }
-
-.hero-image {
-  background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("backgroundimg.jpg");
-  height: 100%;
+.hero-texts {
+  height: 50%;
   background-position: center;
   background-repeat: no-repeat;
   background-size: cover;
   position: relative;
 }
+.img-container {
+    background-image: url("zhomepage/images/heroimag.png");
+    height: 50%;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    position: relative;
+}
 
-.hero-text {
+.hero-texted {
   text-align: center;
   position: absolute;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-  color: white;
+  color: grey;
+  line-height: 1.6;
 }
 @media only screen and (max-width: 800px) {
  .hero-text h1{
     font-size:40px;
 
  }
- h1 img{
-    height:50px;
- }
+ 
 }
 
 
 </style>
 </head>
 <body>
-<div class="hero-image">
-  <div class="hero-text">
-    <h1 style="font-size:60px"><img src="assets/images/logokam.png" alt="kamchettra logo" height="70px">| KAM- क्षॆत्र </h1>
-    <i style="font-size:25px;">"Goals are dreams with deadlines."</i><br><br><br>
-    <a href="AdminIndex.php"><button style="background-color:transparent;padding:15px 28px;border-radius:5px;color:white; cursor: pointer;font-size:18px;transition: all 0.5s;">ADMIN</button></a> &nbsp;&nbsp;
+  <!-- navbar starts here! -->
+  <div class="nav-top">
+        <p class="number"><i class="fas fa-mobile-alt"></i> 9810080214</p>
+        <p><i class="far fa-envelope"></i> info@kamchettra.com</p>
+        <span>  <i class="fab fa-facebook-f"></i>
+        <i class="fab fa-instagram"></i>
+        <i class="fab fa-google-plus-g"></i>
+        <i class="fab fa-linkedin-in"></i>
+        <i class="fab fa-twitter"></i></span>
+    </div>
+    <hr>
+    <nav>
+        <div class="logo">
+            <img src="assets/images/logokam.png" alt="mt" height="60px" width="60px">
+        </div>
+        <ul class="nav-links">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+        <div class="burger">
+            <div class="line1"></div>
+            <div class="line2"></div>
+            <div class="line3"></div>
+        </div>
+    </nav>
+<div class="hero-texts">
+  <div class="hero-texted">
+    <h1 >Organize your <br> work and life, finally with KAM- क्षॆत्र. </h1>
+    <p>
+    <i style="font-size:15px;">"Goals are dreams with deadlines."</i>
+    </p><br>
+    <a href="#"><button class="bookbtn">GET STARTED !</button></a>
+    <!-- <a href="AdminIndex.php"><button style="background-color:transparent;padding:15px 28px;border-radius:5px;color:white; cursor: pointer;font-size:18px;transition: all 0.5s;">ADMIN</button></a> &nbsp;&nbsp;
     <a href="user/Index.php"><button style="background-color:transparent;padding:15px 28px;border-radius:5px;color:white; cursor: pointer;font-size:18px;transition: all 0.5s;">USER</button></a>
-         
+          -->
   </div>
 </div>
+<div class="img-container">
+
+</div>
+<!-- Here starts the content -->
+<div class="specifica">
+        <h1 class="specific"> Why <span class="specification"> KAM-क्षॆत्र?</span> </h1>
+</div>
+<!-- Here starts the card section -->
+<div class="card-flex">
+  <div class="column1">
+    <i class="uil  uil-arrow-growth"></i>
+    <h2>Increase productivity</h2>
+    <p>Teams using KAM-CHETTRA are 45% more efficient. Automate routine work. Turn communication into action. And bring good ideas to life faster.</p>
+  </div>
+  <div class="column2">
+  <i class="uil uil-clipboard-notes"></i>
+  <h2>Streamline management</h2>
+    <p>According to 82% of customers, KAM_-CHETTRA gives teams greater clarity. Because everyone knows the plan, the process, and where work stands.</p>
+ 
+  </div>
+  <div class="column3">
+  <i class="uil uil-crosshairs"></i>
+  <h2>Improve visibility</h2>
+    <p>80% of customers say KAM-CHETTRA increases accountability. With everything teams need to know and do in one place, you can stay aligned on responsibilities and goals.</p>
+ 
+  </div>
+</div>
+<!-- services -->
+<div class="services-holder">
+<div class="explore">
+        <h1 class="exploresp">EXPLORE OUR <span class="specification">SERVICES</span> 
+        </h1>
+        <p class="budget">CHOOSE YOUR ENVIRONMENT</p>
+        <div class="card">
+            <div class="card1">
+                <img src="zhomepage/images/happygirl.jpg" alt="card1" class="card01">
+                <h2 class="budgeth2">FREE TRIAL</h2>
+                <p class="budgeth2">Free trial for 1 month!</p>
+                <div class="dividerr"></div>
+                <div class="content-btn">
+                    <p class="budgeth2"><span>$0 /</span> month</p>
+                    <button class="btn-dtls">VIEW DETAILS</button>
+                </div>
+            </div>
+            <div class="card1">
+                <img src="zhomepage/images/threepeoples.jpg" alt="card1" class="card01">
+                <h2 class="budgeth2">Medium Ranged</h2>
+                <p class="budgeth2">For small circles</p>
+                <div class="dividerr"></div>
+                <div class="content-btn">
+                    <p class="budgeth2"><span>$50 /</span> month</p>
+                    <button class="btn-dtls">VIEW DETAILS</button>
+                </div>
+            </div>
+            <div class="card1">
+                <img src="zhomepage/images/many.jpg" alt="card1" class="card01">
+                <h2 class="budgeth2">Premium Package</h2>
+                <p class="budgeth2">For large offices</p>
+                <div class="dividerr"></div>
+                <div class="content-btn">
+                    <p class="budgeth2"><span>$170 /</span> months</p>
+                    <button class="btn-dtls">VIEW DETAILS</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+    </div>
+<!-- Contact us page starts here-->
+<div class="contact-container">
+<h1 class="exploresp">CONTACT <span class="specification">US</span><i class="uil uil-envelope-alt"></i> </h1> <br><br><br><br>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d467.9361578368045!2d85.30845253927635!3d27.67453807502398!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb1833e6b3e205%3A0x76ca1abd0d699e69!2sDAV%20College!5e0!3m2!1sen!2snp!4v1656787303855!5m2!1sen!2snp" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+</div>
+<!-- Contact us page ends here-->
+<script src="zhomepage/js/apps.js"></script>
 </body>
 </html>
